2fbcd1734157d40845899a64fd21a6645262c6bb
dharunsri09 <115633964+dharunsri09@users.noreply.github.com>
2023-05-04 17:40:53 +0530

    Add files via upload

---
+++ Billing_System.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package billing_system;
+
+/**
+ *
+ * @author kongu
+ */
+public class Billing_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
